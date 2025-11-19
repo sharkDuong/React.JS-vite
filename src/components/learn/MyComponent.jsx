e9bd426dd1c63d05788a2a7f9ce5d8f424b09f3e
sharkDuong <phantohiroxi@gmail.com>
2025-11-20 00:29:47 +0700

    vd31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,13 @@
 import './style.css'
 
 const MyComponent = () => {
+  const ngocduong = {
+    name: "NgocDuongDev",
+    age: 23
+  };
   return (
     <>
-    <div> React & NgocDuongDev</div>
+    <div> React & {JSON.stringify(ngocduong)}</div>
     <div className="child" 
      style={{ borderRadius:"10px" }}
     >child</div>
