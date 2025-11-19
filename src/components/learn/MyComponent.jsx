ae1939c619a1b96066fcc3306a4488cc4615deac
sharkDuong <phantohiroxi@gmail.com>
2025-11-20 00:15:59 +0700

    vd30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,13 @@
+import './style.css'
+
 const MyComponent = () => {
   return (
+    <>
     <div> React & NgocDuongDev</div>
+    <div className="child" 
+     style={{ borderRadius:"10px" }}
+    >child</div>
+    </>
   )
 }
 export default MyComponent
