0146eb5c437a20c4e9f8daeb69f5cbbcd8048f66
sharkDuong <phantohiroxi@gmail.com>
2025-11-21 11:08:03 +0700

    vd48

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import TodoData from "./components/todo/TodoData";
 import TodoNew from "./components/todo/TodoNew";
 import reactLogo from "./assets/react.svg";
 import { useState } from "react";
+import Header from "./components/layout/header";
+import Footer from "./components/layout/footer";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   const [todoList, setTodoList] = useState([
@@ -28,26 +31,31 @@ const App = () => {
     setTodoList(updatedTodoList);
   };
   return (
-    <div className="todo-container">
-      <div className="todo-title">Todo List</div>
-      <TodoNew addNewTodo={addNewTodo} />
+    <>
+      <Outlet />
+      <Header />
+      <div className="todo-container">
+        <div className="todo-title">Todo List</div>
+        <TodoNew addNewTodo={addNewTodo} />
 
-      {todoList.length > 0 ? (
-        <TodoData todoList={todoList} deleteTodo={deleteTodo} />
-      ) : (
-        <div className="todo-image">
-          <img src={reactLogo} className="logo react" />
-        </div>
-      )}
+        {todoList.length > 0 ? (
+          <TodoData todoList={todoList} deleteTodo={deleteTodo} />
+        ) : (
+          <div className="todo-image">
+            <img src={reactLogo} className="logo react" />
+          </div>
+        )}
 
-      {/* {todoList.length > 0 && <TodoData todoList={todoList} />}
+        {/* {todoList.length > 0 && <TodoData todoList={todoList} />}
 
       {todoList.length === 0 && (
         <div className="todo-image">
           <img src={reactLogo} className="logo react" />
         </div>
       )} */}
-    </div>
+      </div>
+      <Footer />
+    </>
   );
 };
 
---
+++ src/components/layout/footer.jsx
@@ -0,0 +1,9 @@
+import "./footer.css";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>React Vite - @NgocDuongDev</p>
+    </div>
+  );
+};
+export default Footer;
